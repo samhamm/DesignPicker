ae8df6821b18ad204d367655473379f3952de470
sueanyv <valenciasue@hotmail.com>
2016-11-08 17:51:15 -0800

    fixing constructors

--- design.js
+++ design.js
@@ -1,28 +1,28 @@
-var
+'use strict';
+nailDesignsImg = [];
 
 
-
-function nailDesigns(   ){
-this.imgName =ImgName;
-this.path = path;
+function nailDesigns(imgName, path) {
+  this.imgName = ImgName;
+  this.path = path;
 }
 
-new nailDesigns( 'beigenblack','nails.jpg/beigenblack.jpg')
-new nailDesigns('blackandwhite' 'nails.jpg/blackandwhite.jpg ')
-new nailDesigns( 'blackswirlnail,s', 'nails.jpg/blackswirl.jpg')
-new nailDesigns('cherryblosnails', 'nails.jpg/cherryblos.jpg')
-new nailDesigns('goldstil.jpg','nails.jpg/goldstil.jpg')
-new nailDesigns(' flowerprint', 'nails.jpg/flowerprint.jpg')
-  new nailDesigns('lines ', 'nails.jpg/lines.jpg')
-new nailDesigns( 'navy','nails.jpg/navy.jpg')
-new nailDesigns( 'neongreen' ,'nails.jpg/neongreen.jpg')
-  new nailDesigns( 'Orange-Metallic-','nails.jpg/Orange-Metallic-.jpg')
-new nailDesigns('redspark.jpg','nails.jpg/redspark.jpg ')
-  new nailDesigns(' redflow','nails.jpg/redflow.jpg')
-new nailDesigns(  ' rednblack','nails.jpg/rednblack.jpg')
-  new nailDesigns(' royal','nails.jpg/royal.jpg')
-  new nailDesigns(   'simple ',' nails.jpg/simple.jpg')
-  new nailDesigns(   'sparkle' ,' nails.jpg/sparkle.jpg')
-    new nailDesigns( 'tiffanyblue' , ' nails.jpg/tiffanyblue.jpg')
-    new nailDesigns( ' whiteandblack',' nails.jpg/whiteandblack.jpg')
-    new nailDesigns( ' whitetri',' nails.jpg/whitetri.jpg')
+new NailDesigns('beigenblack', 'nails.jpg/beigenblack.jpg');
+new NailDesigns('blackandwhite', 'nails.jpg/blackandwhite.jpg ');
+new NailDesigns('blackswirlnail,s', 'nails.jpg/blackswirl.jpg');
+new NailDesigns('cherryblosnails', 'nails.jpg/cherryblos.jpg');
+new NailDesigns('goldstil.jpg', 'nails.jpg/goldstil.jpg');
+new NailDesigns(' flowerprint', 'nails.jpg/flowerprint.jpg');
+new NailDesigns('lines ', 'nails.jpg/lines.jpg');
+new NailDesigns('navy', 'nails.jpg/navy.jpg');
+new NailDesigns('neongreen', 'nails.jpg/neongreen.jpg');
+new NailDesigns('Orange-Metallic-', 'nails.jpg/Orange-Metallic-.jpg');
+new NailDesigns('redspark.jpg', 'nails.jpg/redspark.jpg ');
+new NailDesigns(' redflow', 'nails.jpg/redflow.jpg');
+new NailDesigns(' rednblack', 'nails.jpg/rednblack.jpg');
+new NailDesigns(' royal', 'nails.jpg/royal.jpg');
+new NailDesigns('simple ', ' nails.jpg/simple.jpg');
+new NailDesigns('sparkle', ' nails.jpg/sparkle.jpg');
+new NailDesigns('tiffanyblue', ' nails.jpg/tiffanyblue.jpg');
+new NailDesigns(' whiteandblack', ' nails.jpg/whiteandblack.jpg');
+new NailDesigns(' whitetri', ' nails.jpg/whitetri.jpg');
