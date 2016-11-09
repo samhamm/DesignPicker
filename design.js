e8ff87e6b27c5d8b194563b41f45d25fb2c415a2
sueanyv <valenciasue@hotmail.com>
2016-11-08 17:42:27 -0800

    added images

---
+++ design.js
@@ -0,0 +1,28 @@
+var
+
+
+
+function nailDesigns(   ){
+this.imgName =ImgName;
+this.path = path;
+}
+
+new nailDesigns( 'beigenblack','nails.jpg/beigenblack.jpg')
+new nailDesigns('blackandwhite' 'nails.jpg/blackandwhite.jpg ')
+new nailDesigns( 'blackswirlnail,s', 'nails.jpg/blackswirl.jpg')
+new nailDesigns('cherryblosnails', 'nails.jpg/cherryblos.jpg')
+new nailDesigns('goldstil.jpg','nails.jpg/goldstil.jpg')
+new nailDesigns(' flowerprint', 'nails.jpg/flowerprint.jpg')
+  new nailDesigns('lines ', 'nails.jpg/lines.jpg')
+new nailDesigns( 'navy','nails.jpg/navy.jpg')
+new nailDesigns( 'neongreen' ,'nails.jpg/neongreen.jpg')
+  new nailDesigns( 'Orange-Metallic-','nails.jpg/Orange-Metallic-.jpg')
+new nailDesigns('redspark.jpg','nails.jpg/redspark.jpg ')
+  new nailDesigns(' redflow','nails.jpg/redflow.jpg')
+new nailDesigns(  ' rednblack','nails.jpg/rednblack.jpg')
+  new nailDesigns(' royal','nails.jpg/royal.jpg')
+  new nailDesigns(   'simple ',' nails.jpg/simple.jpg')
+  new nailDesigns(   'sparkle' ,' nails.jpg/sparkle.jpg')
+    new nailDesigns( 'tiffanyblue' , ' nails.jpg/tiffanyblue.jpg')
+    new nailDesigns( ' whiteandblack',' nails.jpg/whiteandblack.jpg')
+    new nailDesigns( ' whitetri',' nails.jpg/whitetri.jpg')
